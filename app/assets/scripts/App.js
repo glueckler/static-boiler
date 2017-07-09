3f0eec69db498dd44a054e1249844965ee727257
Dean Pete <lankypete7@gmail.com>
2017-07-09 14:43:16 -0600

    Added RevealOnScrool js module

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,4 @@
+import $ from 'jquery';
+import RevealOnScroll from "./modules/RevealOnScroll";
 
+new RevealOnScroll($(".site-footer"),"100%");
--- app/temp/scripts/Vendor.js
+++ app/temp/scripts/Vendor.js
@@ -60,23 +60,26 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */,
-/* 1 */
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(2);
+__webpack_require__(5);
 
-__webpack_require__(3);
+__webpack_require__(6);
 
 /***/ }),
-/* 2 */
+/* 5 */
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -778,7 +781,7 @@ __webpack_require__(3);
 
 
 /***/ }),
-/* 3 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_RESULT__;/*! picturefill - v3.0.2 - 2016-02-12
